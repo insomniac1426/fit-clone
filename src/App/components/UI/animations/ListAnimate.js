d4a4b6ea1e3f897189064131937422416183cb46
yashbansal-zomato <yash.bansal@zomato.com>
2021-05-22 22:45:10 +0530

    removed console

--- src/App/components/UI/animations/ListAnimate.js
+++ src/App/components/UI/animations/ListAnimate.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import { FlexBox } from "../atoms/FlexBox";
 
 const ListAnimate = ({ animationCss, delay, initalDelay = 0, children, className }) => {
-  console.log({ children });
   const AnimatedChildren = React.Children.map(children, (child, index) => {
     const animationDelay = initalDelay + index * delay;
     return (
